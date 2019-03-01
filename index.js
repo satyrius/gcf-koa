0d804f750d35948ab8acd36c97b15cbb2422f169
Anton Egorov <anton@mothership.cx>
2019-03-01 21:25:16 +0200

    hello world from koa

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-exports.helloGET = (req, res) => {
-  res.send("Hello World!");
-};
+const Koa = require("koa");
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = "Hello World from Koa";
+});
+
+exports.helloGET = app.callback();
